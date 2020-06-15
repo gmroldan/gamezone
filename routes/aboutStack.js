982b6e8be3a3d55405ccfafd665b0f3630b3324f
martin <gmroldanutn@gmail.com>
2020-06-15 17:52:20 -0300

    Custom Header Component

--- routes/aboutStack.js
+++ routes/aboutStack.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import About from '../screens/about';
+import Header from '../shared/header';
 
 const Stack = createStackNavigator();
 
-export default function AboutStack() {
+export default function AboutStack({ navigation }) {
   return (
     <Stack.Navigator initialRouteName="About" screenOptions={{ headerStyle: { backgroundColor: '#eee' } }}>
-      <Stack.Screen name="About" component={About} />
+      <Stack.Screen name="About" options={{ headerTitle: () => <Header navigation={ navigation } title='About GameZone' /> }} component={About} />
     </Stack.Navigator>
   );
 }
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
+import Header from '../shared/header';
 
 const Stack = createStackNavigator();
 
-export default function HomeStack() {
+export default function HomeStack({ navigation }) {
   return (
     <Stack.Navigator initialRouteName="Home" screenOptions={{ headerStyle: { backgroundColor: '#eee' } }}>
-      <Stack.Screen name="Home" options={{ title: 'GameZone' }} component={Home} />
+      <Stack.Screen name="Home" options={{ headerTitle: () => <Header navigation={ navigation } title='GameZone' /> }} component={Home} />
       <Stack.Screen name="ReviewDetails" options={{ title: 'Review Details' }} component={ReviewDetails} />
     </Stack.Navigator>
   );
